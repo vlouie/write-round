5d701f94db847542579d33f7e8d478e85bf48e68
Victoria Louie <v.louie91@gmail.com>
2015-03-20 21:50:11 -0700

    Make this sql script work

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS user (
 	id int NOT NULL AUTO_INCREMENT,
-	username varchar2(50) NOT NULL,
-	password varchar2(50) NOT NULL,
-	email varchar2(50) NOT NULL,
+	username varchar(50) NOT NULL,
+	password varchar(50) NOT NULL,
+	email varchar(50) NOT NULL,
 	created_at datetime NOT NULL,
 	PRIMARY KEY ( id ),
 	CONSTRAINT uniq_user UNIQUE ( username )
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS user (
 
 CREATE TABLE IF NOT EXISTS session (
 	id int NOT NULL AUTO_INCREMENT,
-	title varchar2(255) NOT NULL,
+	title varchar(255) NOT NULL,
 	current_turn int NOT NULL,
 	created_at datetime NOT NULL,
 	updated_at datetime NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS session (
 CREATE TABLE IF NOT EXISTS story (
 	id int NOT NULL AUTO_INCREMENT,
 	session_id int NOT NULL,
-	text longtext CHARACTER SET utf-8,
+	text longtext CHARACTER SET utf8,
 	PRIMARY KEY ( id ),
 	CONSTRAINT fk_session FOREIGN KEY ( session_id ) REFERENCES session( id )
 );
@@ -29,8 +29,8 @@ CREATE TABLE IF NOT EXISTS story (
 CREATE TABLE IF NOT EXISTS relation (
 	user_id1 int NOT NULL,
 	user_id2 int NOT NULL,
-	type varchar2(30) DEFAULT NULL,
-	CHECK(type IN ('friends', 'block'),
+	type varchar(30) DEFAULT NULL,
+	CHECK(type IN ('friends', 'block')),
 	PRIMARY KEY ( user_id1, user_id2),
 	CONSTRAINT fk_user_1 FOREIGN KEY ( user_id1 ) REFERENCES user( id ),
 	CONSTRAINT fk_user_2 FOREIGN KEY ( user_id2 ) REFERENCES user( id )
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS relation (
 CREATE TABLE IF NOT EXISTS users_sessions (
 	user_id int NOT NULL,
 	session_id int NOT NULL,
-	colour varchar2(30) NOT NULL,
+	colour varchar(30) NOT NULL,
 	turn int NOT NULL,
 	PRIMARY KEY ( user_id, session_id ),
 	CONSTRAINT fk_user_session FOREIGN KEY ( user_id ) REFERENCES user( id ),
