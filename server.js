d5c3ebf1fef6e49a183fdc7f8cc16fd23ec8fdbd
Victoria Louie <v.louie91@gmail.com>
2015-03-21 11:48:09 -0700

    Testing server-database connection; updating packages

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
 var express    = require('express');
+var session    = require('express-session');
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'passw0rd',
-  database : 'write_round'
+    host     : 'localhost',
+    user     : 'root',
+    password : 'passw0rd',
+    database : 'write_round'
 });
 
-connection.connect();
+var app = express();
 
-connection.query('SELECT * from users', function(err, rows, fields) {
-  if (!err)
-    console.log('The solution is: ', rows);
-  else
-    console.log('Error while performing Query.');
+app.use(express.static('./www'));
+
+connection.connect(function(err){
+    if(!err) {
+        console.log("Database is connected ... \n\n");
+    } else {
+        console.log("Error connecting database ... \n\n");
+    }
 });
 
-connection.end();
+app.get("/",function(req,res){
+    connection.query('SELECT * from user', function(err, rows, fields) {
+    connection.end();
+    if (!err)
+      console.log('The solution is: ', rows);
+    else
+      console.log('Error while performing Query.');
+    });
+});
+
+app.listen(3000);
+//connection.connect();
+
+//connection.query('SELECT * from user', function(err, rows, fields) {
+  //if (!err)
+    //console.log('The solution is: ', rows);
+  //else
+    //console.log('Error while performing Query.');
+//});
+
+//connection.end();
