d0a61d0ae4f82b9432ebf313b1bf4b7e4bcfeda7
Victoria Louie <v.louie91@gmail.com>
2015-04-01 00:52:24 -0700

    Upping password length

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS user (
 	id int NOT NULL AUTO_INCREMENT,
 	username varchar(50) NOT NULL,
-	password varchar(50) NOT NULL,
+	password varchar(255) NOT NULL,
 	email varchar(50) NOT NULL,
 	created_at datetime NOT NULL,
 	PRIMARY KEY ( id ),
